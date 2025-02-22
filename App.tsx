fbb9c879c9b424026c4294b7d9cd84ede725f832
Pedro Laurentino Okuma <65199541+Okuma-wq@users.noreply.github.com>
2025-02-22 00:51:29 +0000

    sync

--- App.tsx
+++ App.tsx
@@ -1,16 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { Image, ImageBackground, StyleSheet, Text, View } from 'react-native';
+import { Alert, Image, ImageBackground, StyleSheet, Text, View } from 'react-native';
+import MeuBotao from './components/MeuBotao';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <ImageBackground style={styles.bgimage} source={{ uri: 'https://i.pinimg.com/originals/ec/35/fd/ec35fd09f1402cf4828c617d30096487.gif'}}>
-      <h1 style={styles.titulo} >THIS IS FINE</h1>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <Image style={styles.image} 
-        source={{ uri:'https://media.tenor.com/ZGXm_DhIu5gAAAAM/this-is-fine-fine.gif'}}></Image>
+      <MeuBotao titulo='Clique aqui' onPress={()=> {Alert.alert("Atenção","Botão Clicado")}}></MeuBotao>
       <StatusBar style="auto" />
-      </ImageBackground>
     </View>
   );
 }
@@ -28,12 +25,5 @@ const styles = StyleSheet.create({
   },
   titulo:{
     color: 'white'
-  },
-  bgimage: {
-    height: '100%',
-    width: '100%',
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
---
+++ components/MeuBotao.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Button } from "react-native";
+import { Colors } from "react-native/Libraries/NewAppScreen";
+
+type MeuBotaoProps = {
+    titulo: string;
+    onPress: () => void;
+}
+
+const MeuBotao:React.FC<MeuBotaoProps>= ({titulo, onPress})=>{
+    return (
+        <Button onPress={onPress} title={titulo} color='#000055'></Button>
+    );
+}
+
+export default MeuBotao; 
